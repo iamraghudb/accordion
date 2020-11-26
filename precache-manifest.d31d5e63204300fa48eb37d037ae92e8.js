e7cf6f2deb189ad8cb12d3fa25fdc014c985652d
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2020-11-26 11:10:15 +0530

    Updates

---
+++ precache-manifest.d31d5e63204300fa48eb37d037ae92e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f320cfb4eb1e9a5669ae397e54d7ea",
+    "url": "/accordion/index.html"
+  },
+  {
+    "revision": "f5f941bfa631092731f1",
+    "url": "/accordion/static/css/main.9280bcb1.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/accordion/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordion/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5f941bfa631092731f1",
+    "url": "/accordion/static/js/main.08109df9.chunk.js"
+  },
+  {
+    "revision": "8bf3a778a198a7a25ab9",
+    "url": "/accordion/static/js/runtime-main.75639503.js"
+  }
+]);
